51619e2924a74a4c6c099911225813959a7852a5
JaewookJung <stich9208@seerslab.com>
2022-08-18 22:24:44 +0900

    초기 test
    Signed-off-by: JaewookJung <stich9208@seerslab.com>

---
+++ api/AuthAPI.d.ts
@@ -0,0 +1,28 @@
+declare global {
+  export namespace AuthAPI {
+    export interface SIGN_IN {
+      req: IAuthAPI.SignInRequestBody;
+      res: IAuthAPI.SignInResponseBody;
+      query: undefined;
+    }
+  }
+}
+
+// ------------ SIGN_IN ------------
+
+export namespace IAuthAPI {
+  interface SignInRequestBody {
+    email: string;
+    password: string;
+  }
+
+  interface SignInResponseBody {
+    message: string;
+    accessToken: string;
+    refreshToken: string;
+    accessTokenExpiry: string;
+    refreshTokenExpiry: string;
+  }
+}
+
+export {};
